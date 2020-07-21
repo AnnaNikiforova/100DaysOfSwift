5204a1d27795ab9fac29b83026e6745489a6e539
Anna Nikiforova <49957977+AnnaNikiforova@users.noreply.github.com>
2020-07-21 18:57:54 +0300

    Add files via upload

---
+++ Project7/Project7/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Анна Никифорова on 20.07.2020.
+//  Copyright © 2020 Анна Никифорова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
+    var keyword = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let urlString: String
+            
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+            parse(json: data)
+                return
+            }
+        }
+        showError()
+    }
+    
+    @IBAction func creditsButtonTapped(_ sender: Any) {
+        let ac = UIAlertController(title: "Credits", message: "Data comes from the We The People API of the Whitehouse.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @IBAction func filterButtonTapped(_ sender: Any) {
+        let ac = UIAlertController(title: "Filter by a keyword", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.addAction(UIAlertAction(title: "OK", style: .default) { [weak self, weak ac] _ in
+            self?.keyword = ac?.textFields?[0].text ?? ""
+            self?.filter()
+            self?.tableView.reloadData()
+        })
+        
+        present(ac, animated: true)
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            filter()
+            tableView.reloadData()
+        }
+    }
+    
+    func filter() {
+         if keyword == "" {
+             filteredPetitions = petitions
+             return
+         }
+         
+         filteredPetitions = petitions.filter() { petition in
+            if petition.title.contains(keyword) {
+                 return true
+             }
+             return false
+         }
+     }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredPetitions.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let petition = filteredPetitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = filteredPetitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
