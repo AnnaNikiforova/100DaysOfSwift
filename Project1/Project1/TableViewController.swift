5a66e612a4bebf60f36a990976bdace305ff21ef
Anna Nikiforova <49957977+AnnaNikiforova@users.noreply.github.com>
2020-08-11 20:09:43 +0300

    Update TableViewController.swift

--- Project1/Project1/TableViewController.swift
+++ Project1/Project1/TableViewController.swift
@@ -17,20 +17,24 @@ class TableViewController: UITableViewController {
         
         title = "Storm Viewer"
         navigationController?.navigationBar.prefersLargeTitles = true
-        
-        let fm = FileManager.default
-        let path = Bundle.main.resourcePath!
-        let items = try! fm.contentsOfDirectory(atPath: path)
-        
-        for item in items {
-            if item.hasPrefix("nssl") {
-                // this is a picture to load
-                pictures.append(item)
-                pictures.sort()
+        loadImages()
+        print(pictures)
+    }
+    
+    func loadImages() {
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            let fm = FileManager.default
+            let path = Bundle.main.resourcePath!
+            let items = try! fm.contentsOfDirectory(atPath: path)
+            
+            for item in items {
+                if item.hasPrefix("nssl") {
+                    // this is a picture to load
+                    self?.pictures.append(item)
+                    self?.pictures.sort()
+                }
             }
         }
-        
-        print(pictures)
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
